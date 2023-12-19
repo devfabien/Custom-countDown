1948bfd63b794714238107a1122e5505a32dde1c
devfabien <ish001fabi@gmail.com>
2023-12-19 12:40:41 +0200

    feat: creating the timer function

--- index.js
+++ index.js
@@ -22,3 +22,24 @@ function displayEndTime(timestamp) {
     minutes < 10 ? "0" : ""
   }${minutes}`;
 }
+
+function timer(seconds) {
+  // clear any existing timers
+  clearInterval(countdown);
+
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+  displayEndTime(then);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    // check if we should stop it!
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    // display it
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
