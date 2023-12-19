9901d9d2af34d5f37e47d4ba8067950eb85b7bbb
devfabien <ish001fabi@gmail.com>
2023-12-19 12:37:47 +0200

    feat: declaring the variables to be used

---
+++ index.js
@@ -0,0 +1,4 @@
+let countdown;
+const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
+const buttons = document.querySelectorAll("[data-time]");
